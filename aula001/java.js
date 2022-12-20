895f259118d1b3b6ae0642bca9df1d36c7c0a9d2
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-20 11:53:42 +0200

    Update java.js

--- aula001/java.js
+++ aula001/java.js
@@ -1,8 +1,3 @@
-let bonusPoint = 50
-console.log(`The variable now it's ${bonusPoint}`)
-bonusPoint =  bonusPoint + 50
-console.log(`The variable now it's ${bonusPoint}`)
-bonusPoint = 25
-console.log(`The variable now it's ${bonusPoint}`)
-bonusPoint =  bonusPoint + 45 
-console.log(`The variable now it's ${bonusPoint}`)
+function add(){
+
+}
