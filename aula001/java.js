d44340cc9be7b6949d399d617912927520c8a46a
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-28 19:58:26 +0200

    Update java.js

--- aula001/java.js
+++ aula001/java.js
@@ -1,8 +1,9 @@
 let count = 0
 
+
 function add(){
-    // let msginc = window.document.getElementById("count-el")
-    // msginc.innerHTML=count += 1
+    let countEl = window.document.getElementById("count-el")
+    countEl.innerText= count += 1
     /* I can crate a variable and than set the value or just set direct */
-    window.document.getElementById('count-el').innerText = count+=1
+    // window.document.getElementById('count-el').innerText = count+=1
 }
