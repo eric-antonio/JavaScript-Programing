d9b280846724a8c7ca5a3d4da842553ed7151b90
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-28 19:53:47 +0200

    Lesson 14
    Appling direct the value

--- aula001/java.js
+++ aula001/java.js
@@ -1,7 +1,8 @@
 let count = 0
 
 function add(){
-    let msginc = window.document.getElementById("count-el")
-    msginc.innerHTML=count += 1
-    // console.log(count += 1)
+    // let msginc = window.document.getElementById("count-el")
+    // msginc.innerHTML=count += 1
+    /* I can crate a variable and than set the value or just set direct */
+    window.document.getElementById('count-el').innerText = count+=1
 }
