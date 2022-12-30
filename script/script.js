e51990b7106ccfd4291ec62baf3c17339fa98a5d
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-30 12:25:04 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,3 +3,4 @@ let welcomeEl =  window.document.getElementById("welcome-el")
 let name = "Eric"
 let greeting = "Hi ,my name is "
 welcomeEl.innerText = ` ${greeting} ${name}`
+welcomeEl.innerText = welcomeEl.innerText + `👍`
