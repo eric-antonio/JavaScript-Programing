a350026638f850f80b8a8ea796da76375ba7525d
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-30 12:09:20 +0200

    Lessoa 22

--- script/script.js
+++ script/script.js
@@ -1,11 +1,5 @@
-let name = "22"
-let greeting = "Hi, my name is "
-let myGreeting = `${greeting}${name}`
-console.log(myGreeting)
+let welcomeEl =  window.document.getElementById("welcome-el")
 
-
-let points = 4
-let bonusPoints = "10"
-
-let totalPoints = points + bonusPoints
-console.log(totalPoints) 
+let name = "Eric"
+let greeting = "Hi ,my name is "
+welcomeEl.innerText = ` ${greeting} ${name}`
