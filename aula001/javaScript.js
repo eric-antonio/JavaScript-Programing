67319d5a2d7be05828c6b468a8b929d351d3e806
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-30 12:41:09 +0200

    Update javaScript.js

--- aula001/javaScript.js
+++ aula001/javaScript.js
@@ -4,11 +4,17 @@ let count = 0
 function add(){
     let countEl = window.document.getElementById("count-el")
     countEl.innerText= count += 1
-    /* I can crate a variable and than set the value or just set direct */
-    // window.document.getElementById('count-el').innerText = count+=1
+
+    /* I can crate a variable and than set the value or just set direct 
+     window.document.getElementById('count-el').innerText = count+=1*/
+
 }
 
 function save(){
-    console.log(count)
+    let saveEl = window.document.getElementById('save-el')
+    let sum =  count + ` - `
+
+    saveEl.innerText += sum
+   
 }
 save()
