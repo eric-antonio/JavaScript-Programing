3a3f1a0d5f5fc8f1af187339cd51a6368fe0cf45
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-30 11:39:12 +0200

    lesson 21 | Number VS Strings
    Number VS Strings

--- script/script.js
+++ script/script.js
@@ -1,4 +1,11 @@
-let name = "Éric"
+let name = "22"
 let greeting = "Hi, my name is "
 let myGreeting = `${greeting}${name}`
 console.log(myGreeting)
+
+
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = points + bonusPoints
+console.log(totalPoints) 
