98c71ddea775331f342d88a7b09ae7362937cc47
Éric Ivan João António <ericantoniodev@gmail.com>
2022-12-31 11:03:31 +0200

    Update javaScript.js

--- aula001/javaScript.js
+++ aula001/javaScript.js
@@ -3,18 +3,13 @@ let countEl = window.document.getElementById("count-el")
 let saveEl = window.document.getElementById('save-el')
 
 function add(){
-    
     countEl.innerText= count += 1
-
     /* I can crate a variable and than set the value or just set direct 
      window.document.getElementById('count-el').innerText = count+=1*/
-
 }
 
 function save(){
-    
     let sum =  count + ` - `
-
     saveEl.innerText += sum
    
 }
