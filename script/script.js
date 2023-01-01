c161ee882eb3cd233bf4320b2f74d4763a659542
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-01 13:40:45 +0200

    Exercise nr 4

--- script/script.js
+++ script/script.js
@@ -16,3 +16,11 @@ removelPoint()
 add3Points()
 
 console.log(myPoints)
+
+
+console.log("2" + 2) // 22
+console.log( 11 + 7 ) // 18
+console.log(6 + "5") // 65
+console.log("My points: " + 5 + 9 )
+console.log( 2 + 2) // 4
+console.log("11" + "14") // 1114
