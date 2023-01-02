8e48c90a0ed923ea249eeb589e56381d0e593d40
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-02 11:41:35 +0200

    Subtract function is working !

--- 3-calculator/index.js
+++ 3-calculator/index.js
@@ -1,19 +1,20 @@
 let num1 =  8
 let num2 = 2
-let sum = 0
+let calculate = 0
 
 window.document.getElementById("num1-el").textContent = num1
 window.document.getElementById("num2-el").textContent = num2
 let result = window.document.getElementById("sum-el")
 
 function add(){
-    sum =  num1 + num2
-    result.textContent = sum
+    calculate =  num1 + num2
+    result.textContent = calculate
 
 }
 
 function subtract(){
-
+    calculate = num1 - num2
+    result.textContent = calculate
 }
 function divide(){
 
