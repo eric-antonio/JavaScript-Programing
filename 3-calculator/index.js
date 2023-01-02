91c1ee8d4441fd666ff50f5927657da8baa0c9e7
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-02 11:37:11 +0200

    Update index.js

--- 3-calculator/index.js
+++ 3-calculator/index.js
@@ -1,11 +1,12 @@
 let num1 =  8
 let num2 = 2
+ let sumEl = 0
 
 window.document.getElementById("num1-el").textContent = num1
 window.document.getElementById("num2-el").textContent = num2
 
 function add(){
-
+    sumEl =  
 }
 
 function subtract(){
