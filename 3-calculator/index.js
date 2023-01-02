1d9080a5be294669f65d742765db6a52c212a119
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-02 11:34:01 +0200

    Creating the functions

--- 3-calculator/index.js
+++ 3-calculator/index.js
@@ -3,3 +3,17 @@ let num2 = 2
 
 window.document.getElementById("num1-el").textContent = num1
 window.document.getElementById("num2-el").textContent = num2
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+function divide(){
+
+}
+function multiply(){
+
+}
