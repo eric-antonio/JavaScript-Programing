c52db3ff22f5fe3ffb3f56bea2a41f5591f71e4c
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-02 15:25:39 +0200

    Update index.js

--- 4 - blackJack/index.js
+++ 4 - blackJack/index.js
@@ -1,7 +1,8 @@
 let firstCard = 10 
 let secondCard = 7 
-let sum =  firstCard + secondCard
+let sum =  firstCard + secondCard 
 let hasBlackJack = false
+let isAlive = true
 
 if ( sum <= 20 ){
     console.log("Do you want to draw a new card?😏")
@@ -10,5 +11,7 @@ if ( sum <= 20 ){
     hasBlackJack =  true
 }else{
     console.log("You're out of the Game!🫡")
+    isAlive = false
 }
- console.log(hasBlackJack)
+ console.log("Win? : "+ hasBlackJack)
+ console.log(`In the game? : ` + isAlive)
