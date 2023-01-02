58939b59020184fd81f25043eb44323e0aacaac3
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-02 11:39:46 +0200

    function add is working!

--- 3-calculator/index.js
+++ 3-calculator/index.js
@@ -1,12 +1,15 @@
 let num1 =  8
 let num2 = 2
- let sumEl = 0
+let sum = 0
 
 window.document.getElementById("num1-el").textContent = num1
 window.document.getElementById("num2-el").textContent = num2
+let result = window.document.getElementById("sum-el")
 
 function add(){
-    sumEl =  
+    sum =  num1 + num2
+    result.textContent = sum
+
 }
 
 function subtract(){
