bbefc86cb0d8e90f2228aeaaa528c5897fa271bc
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-04 18:19:28 +0200

    another CHALENGE

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-let messages = [
-    "Hey, how's it going?",
-    "I'm great , Thank you! How about you?",
-    "All good. Been working on my portfolio lately."
-]
-let newMessage = "Same Here!"
+// let messages = [
+//     "Hey, how's it going?",
+//     "I'm great , Thank you! How about you?",
+//     "All good. Been working on my portfolio lately."
+// ]
+// let newMessage = "Same Here!"
 
-messages.push(newMessage)
-for(i = 0 ; i < messages.length ; i++){
-    console.log(`Na posição : ${i} temos : ${messages[i]} `)
+// messages.push(newMessage)
+// for(i = 0 ; i < messages.length ; i++){
+//     console.log(`Na posição : ${i} temos : ${messages[i]} `)
+// }
+
+
+let card =[ 7,5,3]
+for(let i = 0; i < card.length ; i++){
+    console.log(card[i])
 }
