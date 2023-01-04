f345a394f148608f763ba934b468d9a70a1de4f8
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-04 18:15:55 +0200

    use for loop to print messages

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
-for(let i = 10 ; i <= 101 ; i += 10){
-    console.log(i)
+let messages = [
+    "Hey, how's it going?",
+    "I'm great , Thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+let newMessage = "Same Here!"
+
+messages.push(newMessage)
+for(i = 0 ; i < messages.length ; i++){
+    console.log(`Na posição : ${i} temos : ${messages[i]} `)
 }
