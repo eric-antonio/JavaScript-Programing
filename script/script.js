8e486d13f3a86780240814414056d09d711f946b
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-04 19:44:50 +0200

    Math.floor( Math.random() * 6)
    The way to create a random numbers in Js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
-let randomNumber  =  Math.random() * 6
+let randomNumber  = Math.floor( Math.random() * 6)
 let flooredNuMBER = Math.floor(3.45632)
-console.log(flooredNuMBER)
+console.log(randomNumber)
