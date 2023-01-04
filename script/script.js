3381e0c0699d6ecd5c5f034c11c27a18037941b7
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-04 19:15:32 +0200

    Lesson 35  section 3

--- script/script.js
+++ script/script.js
@@ -11,10 +11,37 @@
 // }
 
 
-let setenceEl =  ["Hello" , "my","name" ,"is","Eric"]
-let greetingEl =  window.document.getElementById('greeting-el')
+// let setenceEl =  ["Hello" , "my","name" ,"is","Eric"]
+// let greetingEl =  window.document.getElementById('greeting-el')
 
 
-for (let i = 0 ; i < setenceEl.length ;  i++){
-    greetingEl.textContent +=  setenceEl[i] + " "
+// for (let i = 0 ; i < setenceEl.length ;  i++){
+//     greetingEl.textContent +=  setenceEl[i] + ""
+// }
+
+let player1Time = 102
+let player2Time = 107
+
+function getFastestRaceTime(){
+    if( player1Time < player2Time){
+
+        return player1Time
+
+    }else if (player2Time < player1Time){
+
+         
+        return player2Time
+    }else{
+        return player1Time
+    }
 }
+let fastestRace = getFastestRaceTime()
+console.log(fastestRace)
+
+function getTotalRaceTime(){
+    return player1Time + player2Time
+}  
+
+let totalRaceTime =   getTotalRaceTime()
+
+console.log(totalRaceTime)
