beca6a3684741e7ac11259c0a6511ca375faa0a7
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-04 23:21:45 +0200

    editing the function start game

--- 4 - blackJack/index.js
+++ 4 - blackJack/index.js
@@ -1,11 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-
-let cards = [ firstCard , secondCard]
-
-let sum =  firstCard + secondCard 
+let cards = []
+let sum =  0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = window.document.getElementById('message-el')
 let sumEl =  window.document.getElementById('sum-el')
@@ -25,6 +21,12 @@ let cardsEl =  window.document.getElementById('cards-el')
 }
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards= [ firstCard, secondCard]
+    sum = firstCard + secondCard
+
     renderGame()
 }
 
