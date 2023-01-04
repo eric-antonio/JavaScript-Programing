15c4b4d6f1a80034c3bd63c63c1cd6914c6e40c8
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-04 10:04:30 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
-let card =  [ 7, 4]
-card.push(6)
-console.log(card)
+let messages = [
+    "Hey, how's it going?",
+    "I'm great , Thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+let newMessage = "Same Here!"
+
+messages.push(newMessage)
+
+console.log(messages)
