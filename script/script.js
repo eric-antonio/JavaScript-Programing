dd5d93af732e5bcf19787197707b9acced1c6e8b
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-05 17:19:12 +0200

    Challenge Object

--- script/script.js
+++ script/script.js
@@ -1,9 +1,20 @@
-let course =  {
-    title : "Learn CSS Grid for free.",
-    lessons: 16,
-    creator: "Per Harald Borgen",
-    length: 63,
-    level : 2,
-    isFree: true,
-    tags: ["html" , "css"]
+// let course =  {
+//     title : "Learn CSS Grid for free.",
+//     lessons: 16,
+//     creator: "Per Harald Borgen",
+//     length: 63,
+//     level : 2,
+//     isFree: true,
+//     tags: ["html" , "css"]
+// }
+// console.log(course.tags[1])
+
+let airbnb ={
+    isAvailable : true,
+    name: "Sr.Eric XXVI Castle",
+    rooms: 23,
+    city: ['Mozambique ',  "Maputo"]
 }
+
+console.log(airbnb.city)
+console.log(airbnb.name)
