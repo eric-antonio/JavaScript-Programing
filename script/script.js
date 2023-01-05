f7a496f522a033306c4e2f9d7307fd1cebb3917f
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-05 21:44:03 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-let person ={
-    name: "Eric",
-    age: 23,
-    country: "Mozambique"
-
+let age = 67
+if(age <= 6){
+    console.log('Free')
+}else if(  age > 6 && age <=17 ){
+    console.log('Child discount')
+}else if( age >= 18 && age <= 26 ){
+    console.log('Student discount')
+}else if( age >= 27 && age <= 66){
+    console.log('Full price')
+}else{
+    console.log('Senior citizen discount ')
 }
-
-function logData(){
-    let personData =`${person.name} is ${person.age} years old and lives in ${person.country}`
-    console.log(personData)
-}
-logData()
