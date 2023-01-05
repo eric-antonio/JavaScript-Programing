d0a5ffdb4b5492e2eac07b135d84887c5b4336dd
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-05 21:33:13 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,12 @@
-// let course =  {
-//     title : "Learn CSS Grid for free.",
-//     lessons: 16,
-//     creator: "Per Harald Borgen",
-//     length: 63,
-//     level : 2,
-//     isFree: true,
-//     tags: ["html" , "css"]
-// }
-// console.log(course.tags[1])
+let person ={
+    name: "Eric",
+    age: 23,
+    country: "Mozambique"
 
-let airbnb ={
-    isAvailable : true,
-    name: "Sr.Eric XXVI Castle",
-    rooms: 23,
-    city: ['Mozambique ',  "Maputo"]
 }
 
-console.log(airbnb.city)
-console.log(airbnb.name)
+function logData(){
+    let personData =`${person.name} is ${person.age} years old and lives in ${person.country}`
+    console.log(personData)
+}
+logData()
