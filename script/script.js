50d14d3680da59d94e95bf99e9a161a7c53c7397
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-05 16:13:21 +0200

    Logical operations 2

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 let hasCompletedCourse = true
 let givesCertificate =  true 
+
+if (hasCompletedCourse ){
+    if(givesCertificate){
+        generateCertificate()
+    }
+}
 function generateCertificate(){
     console.log("Generating certificate.....")
 }
