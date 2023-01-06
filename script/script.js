b637c4fe21720327bc64bb830986eae5b9c6b7a9
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-06 11:11:09 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,1 @@
-let largesCountries = ["Tuvalu" , "India", "USA","Indonesia", "Monaco"]
-
-console.log("The 5 largest countries in the world:")
-largesCountries.shift()
-largesCountries.unshift("China")
-
-for(let i = 0 ; i < largesCountries.length; i++){
-    console.log(`- ${largesCountries[i]}`)
-}
-console.log("The 5 largest countries in the world:")
-largesCountries.pop()
-largesCountries.push("Monaco")
-
-for(let i = 0 ; i < largesCountries.length; i++){
-    console.log(`- ${largesCountries[i]}`)
-}
-
-
+let day = 13
