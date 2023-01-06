1675562353588243935a9d6be4f044d88e7a393b
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-06 09:40:39 +0200

    Add new valuers and remove
    shift() -> Remove the elements in the first position .
    unshift() -> Add in the first position
    
    pop()-> Remove the last position.
    push() -> Add in the last position

--- script/script.js
+++ script/script.js
@@ -1,6 +1,18 @@
-let largesCountries = ["China" , "India", "USA","Indonesia", "Pakistan"]
+let largesCountries = ["Tuvalu" , "India", "USA","Indonesia", "Monaco"]
 
 console.log("The 5 largest countries in the world:")
+largesCountries.shift()
+largesCountries.unshift("China")
+
 for(let i = 0 ; i < largesCountries.length; i++){
     console.log(`- ${largesCountries[i]}`)
 }
+console.log("The 5 largest countries in the world:")
+largesCountries.pop()
+largesCountries.push("Monaco")
+
+for(let i = 0 ; i < largesCountries.length; i++){
+    console.log(`- ${largesCountries[i]}`)
+}
+
+
