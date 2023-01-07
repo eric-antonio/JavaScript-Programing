af315a837c9266949f4b2627ce8cc65d04876112
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-07 22:52:59 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,14 @@
-let fruut = ["Apple" ,  "Orange" , "Apple" , "Apple" , "Orange" ]
+let fruit = ["Apple" ,  "Orange" , "Apple" , "Apple" , "Orange" ]
 let appleShelf =  window.document.getElementById('apple-shelf')
 let orangeShelf = window.document.getElementById("orange-shelf")
 
 function or(){
-    for
+    for(let i = 0; i < fruit.length; i ++){
+        if(fruit[i] === "Apple"){
+            appleShelf.textContent += ` ${fruit[i]}  `
+        }else if( fruit[i] ===  "Orange"){
+            orangeShelf.textContent += ` ${fruit[i]}  `
+        }
+    }
 }
+or()
