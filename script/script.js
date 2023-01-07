9625c2eccd47d0fe908916c50d48d56413f4f66d
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-07 22:56:53 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
-let fruit = ["Apple" ,  "Orange" , "Apple" , "Apple" , "Orange" ]
+let fruit = ["🍎" ,  "🍊" , "🍎" , "🍎" , "🍊" ]
 let appleShelf =  window.document.getElementById('apple-shelf')
 let orangeShelf = window.document.getElementById("orange-shelf")
 
 function sortFruit(){
     for(let i = 0; i < fruit.length; i ++){
-        if(fruit[i] === "Apple"){
+        if(fruit[i] === "🍎"){
             appleShelf.textContent += ` ${fruit[i]}  `
-        }else if( fruit[i] ===  "Orange"){
+        }else if( fruit[i] ===  "🍊"){
             orangeShelf.textContent += ` ${fruit[i]}  `
         }
     }
