e56041679c456a94cbb4547bba53c2e656c56806
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-09 10:20:05 +0200

    Challenge const
    Trying to figure out witch variables need to be changed to const

--- Pratice here/script.js
+++ Pratice here/script.js
@@ -1,5 +1,11 @@
-let boxEl =  window.document.getElementById('box')
+let basePrice =  520
+let discount = 120 
+let shippingCost =  12
+let shippingTime =  "5-12 days"
 
-boxEl.addEventListener('click', function(){
-    console.log("I'm here B*tch")
-})
+shippingCost = 15
+shippingTime = "7-14 days"
+
+let fullPrice =  basePrice -  discount + shippingCost
+
+console.log(`Total cost: $${fullPrice} Ti will arrive in ${shippingTime}`)
