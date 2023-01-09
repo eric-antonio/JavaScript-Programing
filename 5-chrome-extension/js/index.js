4461be17b2dff09401212851a968abb46bf697d8
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-09 12:29:36 +0200

    Now we can introduce our leads from the input

--- 5-chrome-extension/js/index.js
+++ 5-chrome-extension/js/index.js
@@ -1,4 +1,4 @@
-let myLeads = [ "www.awesomeLead.com", "www.epicLead.com"," www.greatLead.com"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn =  window.document.getElementById("input-btn")
 const ulEl = window.document.getElementById("list-el")
@@ -6,11 +6,17 @@ const ulEl = window.document.getElementById("list-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+
+    renderLeads()
 })
 
-let  listItems = ""
-for(let i = 0; i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
 
+function renderLeads(){
+    let  listItems = ""
+    for(let i = 0; i < myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>"
+
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
+
