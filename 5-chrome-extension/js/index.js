e924572dada76ab014e9c89bac1e23d3917d1bef
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-09 10:12:27 +0200

    Introducing const
    The new way to declare variables

--- 5-chrome-extension/js/index.js
+++ 5-chrome-extension/js/index.js
@@ -1,4 +1,6 @@
-let inputBtn =  window.document.getElementById("input-btn")
+let myLeads = []
+const inputEl =  window.document.getElementById("input-el") 
+const inputBtn =  window.document.getElementById("input-btn")
 inputBtn.addEventListener('click',function(){
     console.log('Comekieeee??')
 })
