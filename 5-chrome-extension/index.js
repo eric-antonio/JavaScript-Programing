2b492ca466f4d12b064e14ef174136c3922412b0
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-11 20:18:44 +0200

    Update index.js

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -5,27 +5,26 @@ let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn =  window.document.getElementById("input-btn")
 const ulEl = window.document.getElementById("list-el")
-
 // * Get the leads from the local Storage
-
 let leadsFromLocalStorage =  JSON.parse(localStorage.getItem("myLeads"))
-
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
 
+//? Function that get the value from the user and converts into a string 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     // ? Saving  Data into local Storage and converting our Array in to a String Type of data 
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
-    console.log(localStorage.getItem("myLeads"))
-    console.log("Here!: "+leadsFromLocalStorage)
+
+
 })
 
+// * Renders out  in a List the Leads!
 function renderLeads(){
     let  listItems = ""
     for(let i = 0; i < myLeads.length; i++){
