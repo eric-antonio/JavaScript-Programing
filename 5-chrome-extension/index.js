03fc67e833708a0eccc0030d8c5544fb91ae07f8
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-12 11:56:57 +0200

    Update index.js

--- 5-chrome-extension/index.js
+++ 5-chrome-extension/index.js
@@ -25,9 +25,13 @@ tabBtn.addEventListener("click", function(){
    
     // });
 
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    chrome.tabs.query({active:true , currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+
+    })
+    
 })
 
 
