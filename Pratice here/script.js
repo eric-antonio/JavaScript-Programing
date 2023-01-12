47d22d434e76b19d4dd64d4a51d7b2fb372aad83
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-12 09:58:32 +0200

    Update script.js

--- Pratice here/script.js
+++ Pratice here/script.js
@@ -1,12 +1,1 @@
-// const welcomeEl = window.document.getElementById("welcome-el")
-// function greetUser(greeting, name){
-//     welcomeEl.textContent = ` ${greeting} ${name}👍`
-// }
-// greetUser("Welcome back" ,  "Eric")
-
-
-function add(n1, n2){
-    return  n1 +n2
-
-}
-console.log(add(3,4))
+ let array = ['eric' , 'antonio']
