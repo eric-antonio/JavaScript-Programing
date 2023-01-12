4091c7df0dffc56ba4c92b48d40f51b74fb15e42
Éric Ivan João António <ericantoniodev@gmail.com>
2023-01-12 13:13:41 +0200

    Update script.js

--- Pratice here/script.js
+++ Pratice here/script.js
@@ -1,6 +1,16 @@
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    }, 
+    {
+        player: "Mark",
+        score: 41
+    }
+]
 
-let  name= 'Eric'
-
-let valor = localStorage.setItem("name",name)
-let res = localStorage.getItem("name")
-console.log(res)
+let btnEl = window.document.getElementById('btn')
+let pEl =  window.document.getElementById('p-el')
+btnEl.addEventListener("click", function(){
+    pEl.textContent = ` ${data[0].player} ${data[0].score}`
+})
